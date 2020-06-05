ab6bd8e06a497faa3141d54399425222aea09633
cd yang <1012892053@qq.com>
2020-06-05 14:33:56 +0800

    add home

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+
+import api from './api/http'
+Vue.prototype.$api = api  // 全局变量，可在所有页面使用
+
 Vue.config.productionTip = false
 
 new Vue({
