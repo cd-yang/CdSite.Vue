461487cbc8f394f133cf74191dd1ec6821ca646d
cdyang <nwpu409@gmail.com>
2020-07-12 22:34:29 +0800

    import partial antd components

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import Antd from "ant-design-vue"
-import "ant-design-vue/dist/antd.less"
+// import Antd from "ant-design-vue";
+// import "ant-design-vue/dist/antd.less";
+import { spin, layout, affix } from "ant-design-vue";
+// import "ant-design-vue/lib/spin/style";
 
 
 import api from './api/http'
@@ -10,7 +12,10 @@ Vue.prototype.$api = api  // 全局变量，可在所有页面使用
 
 Vue.config.productionTip = false
 
-Vue.use(Antd);
+// Vue.use(Antd);
+Vue.use(spin);
+Vue.use(layout);
+Vue.use(affix);
 
 new Vue({
   router,
